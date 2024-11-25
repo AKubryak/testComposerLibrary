1d62f33506130b4f6ca871aaba8009039b0c99ff
Aleksey Kubryak <aleksey.kubryak@gmail.com>
2024-11-25 21:25:42 +0300

    lesson 33: TestComposerLibrary

---
+++ src/SecondLevel/SecondLevel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Akubryak\TestComposerLibrary\SecondLevel;
+
+class SecondLevel
+{
+    public function test()
+    {
+        echo '2ndLevel';
+    }
+}
---
+++ src/SimpleClass.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Akubryak\TestComposerLibrary;
+
+class SimpleClass
+{
+    public function test()
+    {
+        echo 'test';
+    }
+}
